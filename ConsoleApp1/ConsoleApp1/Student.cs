2ab6059a148516f3339da7698b039c13af833a81
Batoy_Nicholo <nicholox08@gmail.com>
2026-06-21 18:29:06 +0800

    Create Student.cs

---
+++ ConsoleApp1/ConsoleApp1/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Student
+{
+    public string Name;
+    public double Grade1;
+    public double Grade2;
+    public double Grade3;
+
+    public double Average()
+    {
+        return (Grade1 + Grade2 + Grade3) / 3;
+    }
+}
